dd7091f6eaf2c9e06a87e081d2663c38c8d57b3a
Oscar Flores <oflores@mbp-oflores.local>
2020-01-08 12:51:04 -0600

    updated the form logic in contact us and the logo at the bottom of the page

--- src/app/feature-modules/contact/contact.component.ts
+++ src/app/feature-modules/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  public contactForm: FormGroup;
+
 
   constructor() { }
 
   ngOnInit() {
+    this.contactForm = new FormGroup({
+      name: new FormControl(''),
+      email: new FormControl(''),
+      message: new FormControl(''),
+      messageType: new FormControl('')
+
+    })
   }
 
+
+  contactMe(contactFormData) {
+    console.log('hey oscar: ', contactFormData)
+  }
 }
