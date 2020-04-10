6c0d77cd83c09159ae71fee048d1253a18598579
Oscar Flores <oflores@captechventures.com>
2020-04-09 22:39:30 -0500

    Update to Angular 9

--- src/app/feature-modules/contact/contact.module.ts
+++ src/app/feature-modules/contact/contact.module.ts
@@ -3,7 +3,8 @@ import { CommonModule } from '@angular/common';
 import { ContactComponent } from './contact.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule, MatSelectModule } from '@angular/material';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 @NgModule({
